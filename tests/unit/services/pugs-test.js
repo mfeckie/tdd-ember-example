636084f2a6299d533a903aabe98e1598ca93b9d9
Martin Feckie <mfeckie@gmail.com>
2015-08-18 10:21:59 +1000

    Add failing test for pug service

---
+++ tests/unit/services/pugs-test.js
@@ -0,0 +1,37 @@
+import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
+import sinon from 'sinon';
+
+var ajaxStub;
+
+moduleFor('service:pugs', 'Unit | Service | pugs', {
+  beforeEach () {
+    ajaxStub = sinon.stub(Ember.$, 'get');
+  },
+  afterEach () {
+    Ember.$.get.restore();
+  }
+});
+
+test('It calls reddit api', function (assert) {
+  var service = this.subject();
+  service.getPugs();
+  assert.ok(ajaxStub.calledOnce);
+});
+
+// test('It calls with the correct arguments', function (assert) {
+//   var service = this.subject();
+//   service.getPugs();
+//   var args = ajaxStub.args[0][0];
+//
+//   assert.equal(args.url, 'http://www.reddit.com/r/pugs.json');
+// });
+
+// test('It calls with dynamic arguments', function (assert) {
+//   var service = this.subject();
+//
+//   service.getDynamic('muggles');
+//   var args = ajaxStub.args[0][0];
+//
+//   assert.equal(args.url, `http://www.reddit.com/r/${'muggles'}.json`);
+// });
